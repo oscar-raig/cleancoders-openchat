d4554afed6a690f08b804c04a5fd1a248ef5e9a8
oscar-raig <oscar.raig@gmail.com>
2018-12-08 10:12:16 +0100

    Renamed IntegrationTest

--- src/test-integration/java/integration/IntegrationTest.java
+++ src/test-integration/java/integration/IntegrationTest.java
@@ -1,14 +1,14 @@
 package integration;
 
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class IT_RegistrationAPI {
+public class IntegrationTest {
 
   @Test
   public void failing_test() {
     assertTrue(false);
   }
-
 }
